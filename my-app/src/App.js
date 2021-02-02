0b08fa4160acb33b319b8325d062a55e8a510899
SebasLee <heechanlee98@gmail.com>
2021-02-01 18:41:12 -0800

    follow along on youtube

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from'react';
-import List from './components/List/List';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div>
-      <List />
+      <header>
+        <h1>My To-Do List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
