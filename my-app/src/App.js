feb408cebc5f96720f40c2e2b70782a13b5cee41
SebasLee <heechanlee98@gmail.com>
2021-02-01 20:19:30 -0800

    progress

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,17 @@
-import React from'react';
+import React, { useState } from'react';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div>
       <header>
         <h1>My To-Do List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
